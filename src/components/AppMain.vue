b33369bda50b7d66df0863be71249f9bb30c0073
Sebastian-ivaniczki <sivaniczki@gmail.com>
2023-01-20 16:39:20 +0100

    add main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -9,6 +9,53 @@ export default {
  }}
 </script>
 
-<template></template>
+<template>
+    <div class="content">
+        <div class="container">
 
-<style scoped></style>
+            <h3>Content goes here</h3>
+        </div>
+    </div>
+    <div class="jumbotron">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <ul>
+                        <li>
+                            <a href=""></a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <img src="../assets/img/dc-logo-bg.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+ .content {
+    height: 130px;
+    background-color: #000;
+    color: #fff;
+}
+
+h3{
+   line-height: 130px; 
+}
+.jumbotron{
+    background-image: url(../assets/img/footer-bg.jpg);
+    height: 350px;
+}
+.row {
+    display: flex;
+}
+.col {
+    width: 50%;
+}
+img{
+    height: 350px;
+}
+
+</style>
